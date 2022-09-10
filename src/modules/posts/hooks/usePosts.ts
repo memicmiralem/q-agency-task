e775f54a308f9dbc7a1d07a03ef5968238c2ce02
memicmiralem <miralem.memic@amygdal.com>
2022-09-10 13:38:58 +0200

    implement onScroll lazy load

--- src/modules/posts/hooks/usePosts.ts
+++ src/modules/posts/hooks/usePosts.ts
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import useSWR from "swr";
 import { fetcher } from "../../../core/api/fetcher";
 import { PostProps } from "../components/post";
@@ -5,15 +6,29 @@ import { PostProps } from "../components/post";
 export interface UsePostsProps {
   page?: number;
   limit?: number;
+  keyword?: string;
 }
 
-export const usePosts = ({ page = 1, limit = 8 }: UsePostsProps) => {
+export const usePosts = ({ page = 1, limit = 10, keyword }: UsePostsProps) => {
+  const [posts, setPosts] = useState([] as PostProps[]);
+
+  useEffect(() => {
+    setPosts([]);
+  }, [keyword]);
+
   const { data, error } = useSWR(
     `/posts?_page=${page}&_limit=${limit}`,
     fetcher
   );
+
+  useEffect(() => {
+    data?.length > 0 && setPosts((prev) => [...prev, ...data]);
+  }, [data]);
+
   return {
-    data: data as PostProps[],
     error,
+    isLoading: !data,
+    posts,
+    hasMore: data?.length !== 0,
   };
 };
--- src/modules/posts/pages/PostsPage.tsx
+++ src/modules/posts/pages/PostsPage.tsx
@@ -1,41 +1,45 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import { PageHeaderWithFilter } from "../../../shared/components/pageHeaderWithFilter";
 import { Post, PostProps } from "../components/post";
 import { usePosts } from "../hooks/usePosts";
 
 export const PostsPage = () => {
   const [keyword, setKeyword] = useState("");
-  const { data, error } = usePosts({});
+  const [page, setPage] = useState(1);
+
+  const { posts, error, isLoading, hasMore } = usePosts({ page, keyword });
 
   const observer = useRef<IntersectionObserver>();
-  const lastElementRef = useCallback((node: any) => {
-    if (observer.current) observer.current.disconnect();
-    observer.current = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        console.log("eo na dnu sam");
-      }
-    });
-    if (node) observer.current.observe(node);
-  }, []);
-  if (error) return <div>failed to load</div>;
-
-  if (!data) return <div>loading...</div>;
+  const lastElementRef = useCallback(
+    (node: Element | null) => {
+      if (isLoading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPage((prev) => prev + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [isLoading, hasMore]
+  );
 
   const onFilterChange = (keyword: string) => {
     setKeyword(keyword);
+    setPage(1);
   };
+
   return (
     <div className="flex flex-col gap-y-4">
       <PageHeaderWithFilter onFilterChange={onFilterChange} />
-      {data?.map((e: PostProps, index) => {
-        return data.length === index + 1 ? (
-          <div key={e.id} ref={lastElementRef}>
-            <Post {...e} />
-          </div>
-        ) : (
-          <Post key={e.id} {...e} />
-        );
-      })}
+      <div>
+        {posts?.map((e: PostProps, i) => (
+          <Post key={i} {...e} />
+        ))}
+        <div ref={lastElementRef}></div>
+        {error && <div>failed to load</div>}
+        {isLoading && <div>loading...</div>}
+      </div>
     </div>
   );
 };
