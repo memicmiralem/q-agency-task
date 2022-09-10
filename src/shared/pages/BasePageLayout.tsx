2b516822d2b417ad491c0b91a8309c56f789a540
memicmiralem <miralem.memic@amygdal.com>
2022-09-10 14:19:52 +0200

    fix horizontal scroll

--- src/shared/pages/BasePageLayout.tsx
+++ src/shared/pages/BasePageLayout.tsx
@@ -4,7 +4,7 @@ export interface BasePageLayoutProps {
 
 export const BasePageLayout = ({ children }: BasePageLayoutProps) => {
   return (
-    <div className="w-screen h-full bg-gray-200 px-6 sm:px-28 md:px-40 lg:px-44 xl:px-80 2xl:px-96 relative">
+    <div className="w-full h-full bg-gray-200 px-6 sm:px-28 md:px-40 lg:px-44 xl:px-80 2xl:px-96 relative">
       <div className="pt-32">{children}</div>
     </div>
   );
