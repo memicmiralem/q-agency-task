d8a7b8d5d03f747eb824cede58637ab46b3d2097
--global <miralem.memic@amygdal.com>
2022-09-13 20:33:57 +0200

    add explanation for strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,23 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
+  /* 
+Strict mode is intentionally commented out because one part of this task was to make every component log its name once, for what is useEffect used internally. We can always undo the comment, it's just for the purpose of a cleaner console.
+
+Link to React docs: https://reactjs.org/docs/strict-mode.html#detecting-unexpected-side-effects
+
+Strict mode can’t automatically detect side effects for you, but it can help you spot them by making them a little more deterministic. This is done by intentionally double-invoking the following functions:
+
+Class component constructor, render, and shouldComponentUpdate methods
+Class component static getDerivedStateFromProps method
+Function component bodies
+State updater functions (the first argument to setState)
+Functions passed to useState, useMemo, or useReducer
+*/
+
   // <React.StrictMode>
   <App propsMessage={MESSAGES.HELLO} />
   // </React.StrictMode>
-  // dodat komentar sa stacka
 );
 
 // If you want to start measuring performance in your app, pass a function
